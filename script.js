144d16767123ab8e9c0a3495428c072a4be4ba52
Aviral Sheoran <aviralsheoran.2002@gmail.com>
2021-09-02 17:02:30 +0530

    completed❤

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ let screenValue = "";
 for (item of buttons) {
   item.addEventListener("click", (e) => {
     buttonText = e.target.innerText;
-    console.log("Botton text is ", bottonText);
+    console.log("Button text is ", buttonText);
 
     if (buttonText == "X") {
       buttonText = "*";
-      screen.value = buttonText;
+      screenValue += buttonText;
+      screen.value = screenValue;
+    } else if (buttonText == "C") {
+      screenValue = "";
+      screen.value = screenValue;
     } else if (buttonText == "=") {
+      screen.value = eval(screenValue);
+    } else if (buttonText == "^") {
+      buttonText = "**";
+      screenValue += buttonText;
+      screen.value = screenValue;
+    } else {
+      screenValue += buttonText;
+      screen.value = screenValue;
     }
   });
 }
